7db7d15dca5a5bfcc4a848b0a34c28ac0a28694f
Aman Kumar Mishra <aman@studio137.co.za>
2026-08-24 23:31:06 +0530

    added event details page

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,16 +1,122 @@
 import { notFound } from "next/navigation";
+import Image from "next/image";
+import BookEvent from "@/components/BookEvent";
+
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const EventDetailItem = ({ icon, alt, label }: { icon: string, alt: string, label: string }) => (
+    <div className="flex-row-gap-2 items-center">
+        <Image src={icon} alt={alt} height={17} width={17} />
+        <p>{label}</p>
+    </div>
+)
+
+const EventAgenda = ({ agendaItems }: { agendaItems: string[] }) => (
+    <div className="agenda">
+        <h2>Agenda</h2>
+        <ul>
+            {agendaItems.map((item) => (
+                <li key={item}>{item}</li>
+            ))}
+        </ul>
+    </div>
+)
+
+const EventTags = ({ tags }: { tags: string[] }) => (
+    <div className="flex flex-row gap-1.5 flex-wrap">
+        {tags.map((tag) => (
+            <div className="pill" key={tag}>{tag}</div>
+        ))}
+    </div>
+)
 
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
     const { slug } = await params;
-    const request = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/events/${slug}`);
-    const { event } = await request.json();
+    const request = await fetch(`${BASE_URL}/api/events/${slug}`);
 
-    if (!event) notFound();
+    if (!request.ok) notFound();
+
+    const bookings = 10;
+
+    const payload = await request.json() as {
+        event?: {
+            description: string;
+            image: string;
+            overview: string;
+            date: string;
+            time: string;
+            location: string;
+            mode: string;
+            agenda: string;
+            audience: string;
+            organizer: string;
+            tags: string;
+        }
+    };
+
+    if (!payload.event) notFound();
+
+    const {
+        description,
+        image,
+        overview,
+        date,
+        time,
+        location,
+        mode,
+        agenda,
+        audience,
+        organizer,
+        tags,
+    } = payload.event;
 
     return (
         <section id="event">
-            <h1>Event Detail: <br /> {slug}</h1>
+            <div className="header">
+                <h1>Event Description</h1>
+                <p>{description}</p>
+            </div>
+            <div className="details">
+                <div className="content">
+                    <Image src={image} alt="Event Banner" width={800} height={800} className="banner" />
+                    <section className="flex-col-gap-2">
+                        <h2>Overview</h2>
+                        <p>{overview}</p>
+                    </section>
+                    <section className="flex-col-gap-2">
+                        <h2>Event Details</h2>
+                        <EventDetailItem icon="/icons/calendar.svg" alt="calendar" label={date} />
+                        <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time} />
+                        <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location} />
+                        <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode} />
+                        <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience} />
+
+                        <EventAgenda agendaItems={JSON.parse(agenda)} />
+
+                        <section className="flex-col-gap-2">
+                            <h2>About the Organizer</h2>
+                            <p>{organizer}</p>
+                        </section>
+
+                        <EventTags tags={JSON.parse(tags)} />
+
+                    </section>
+                </div>
+                <aside className="booking">
+                    <div className="signup-card">
+                        <h2>Book Your Spot</h2>
+                        {bookings > 0 ? (
+                            <p className="text-sm">
+                                Join {bookings} people who have already booked their spot!
+                            </p>
+                        ) : (
+                            <p className="text-sm">Be the first to book your spot!</p>
+                        )}
+
+                        <BookEvent />
+                    </div>
+                </aside>
+            </div>
         </section>
     )
 }
